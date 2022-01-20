613e6f1008a7b676d511a4e2dfca659cdd462801
juancruz93 <juanklow93@gmail.com>
2022-01-20 16:36:07 -0500

    asube
    sube cambios

---
+++ index.js
@@ -0,0 +1,76 @@
+require('dotenv').config();
+const express = require('express');
+const chalk = require('chalk');
+const webpack = require('webpack');
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const historyApiFallback = require('connect-history-api-fallback');
+const compression = require('compression');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const path = require('path');
+const helmet = require('helmet');
+
+const keys = require('./config/keys');
+
+const routes = require('./routes');
+const socket = require('./socket');
+
+const { database, port } = keys;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    frameguard: true
+  })
+);
+app.use(cors());
+
+// Connect to MongoDB
+mongoose.set('useCreateIndex', true);
+mongoose
+  .connect(database.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(() =>
+    console.log(`${chalk.green('✓')} ${chalk.blue('MongoDB Connected!')}`)
+  )
+  .catch(err => console.log(err));
+
+require('./config/passport')(app);
+app.use(routes);
+
+// if development
+if (process.env.NODE_ENV !== 'production') {
+ 
+  app.use(
+    historyApiFallback({
+      verbose: false
+    })
+  );
+
+
+
+  app.use(express.static(path.resolve(__dirname, '../dist')));
+} else {
+  app.use(compression());
+  app.use(express.static(path.resolve(__dirname, '../dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../dist/index.html'));
+  });
+}
+
+const server = app.listen(port, () => {
+  console.log(
+    `${chalk.green('✓')} ${chalk.blue(
+      `Listening on port ${port}. Visit http://localhost:${port}/ in your browser.`
+    )}`
+  );
+});
+
+socket(server);
